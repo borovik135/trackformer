79f5249326d6f2f3853814dfd07d6a8980f25ba9
Eugene Borovikov <eugene.borovikov@kitware.com>
2024-05-15 17:08:03 -0400

    correct out/log filenames

--- run_track_public.sh
+++ run_track_public.sh
@@ -2,4 +2,5 @@
 python src/track.py with reid\
   tracker_cfg.public_detections=min_iou_0_5\
   obj_detect_checkpoint_file=models/mot17_deformable_multi_frame/checkpoint_epoch_50.pth\
-  2> logs/run_track_private.log
+  > logs/run_track_public.out\
+  2> logs/run_track_public.log
--- srun_track.sh
+++ srun_track.sh
@@ -1,2 +1,2 @@
 srun -p community --gres=gpu:1 --ntasks=1 --ntasks-per-node=1 --cpus-per-task=4\
-  --mem=16G -l -o logs/run_track_private.out $1 &
+  --mem=16G -l -o logs/srun_track.out $1 &
