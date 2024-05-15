07a7fad824d444fbc7da8d3d250e32efdd7c6623
Eugene Borovikov <eugene.borovikov@kitware.com>
2024-05-15 18:01:10 -0400

    add run track demo

---
+++ run_track_demo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+python src/track.py with\
+  dataset_name=DEMO\
+  data_root_dir=data/snakeboard\
+  output_dir=data/snakeboard\
+  write_images=pretty\
+  > logs/run_track_demo.out\
+  2> logs/run_track_demo.log
