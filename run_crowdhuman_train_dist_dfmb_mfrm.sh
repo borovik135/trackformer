3c796edc3b670aa434fcba37cd366fd5f8244f9b
Eugene Borovikov <eugene.borovikov@kitware.com>
2024-05-18 09:58:48 -0400

    correct out and err filenames

--- run_crowdhuman_train_dist_dfmb_mfrm.sh
+++ run_crowdhuman_train_dist_dfmb_mfrm.sh
@@ -1,9 +1,9 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env \
+python -m torch.distributed.launch --nproc_per_node=2 --use_env \
 src/train.py with \
   crowdhuman \
   deformable \
   multi_frame \
   tracking \
   output_dir=models/crowdhuman_train_deformable_multi_frame \
->logs/run_crowdhuman_train_dist_deformable_multi_frame.out \
-2>logs/run_crowdhuman_train_dist_deformable_multi_frame.err
+>logs/run_crowdhuman_train_dist_dfmb_mfrm.out \
+2>logs/run_crowdhuman_train_dist_dfmb_mfrm.err
